b4c3148b338585f8cd905c6aa3d2e564341fc3b0
Cheel <44915367+miche715@users.noreply.github.com>
2025-02-07 16:48:56 +0900

    FEATURE: 초기화.

---
+++ tech-ensemble-main/src/test/java/com/miche/techensemblemain/TechEnsembleMainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miche.techensemblemain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechEnsembleMainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
