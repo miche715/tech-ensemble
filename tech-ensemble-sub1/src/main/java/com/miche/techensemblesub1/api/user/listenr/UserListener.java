fad069983c2190b1af1621b4d777c4d8399408ea
Cheel <44915367+miche715@users.noreply.github.com>
2025-02-09 17:55:41 +0900

    FEATURE: sub1 프로젝트 컨슈머 화.

---
+++ tech-ensemble-sub1/src/main/java/com/miche/techensemblesub1/api/user/listenr/UserListener.java
@@ -0,0 +1,22 @@
+package com.miche.techensemblesub1.api.user.listenr;
+
+import com.miche.techensemblesub1.api.user.service.UserService;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserListener {
+    private UserService userService;
+
+    @Autowired
+    public UserListener(UserService userService) {
+        this.userService = userService;
+    }
+
+    @KafkaListener(topics = "topic-100", groupId = "group-100")
+    public void getUserList(ConsumerRecord<String, String> consumerRecord) {
+        System.out.println(userService.getUserList());
+    }
+}
